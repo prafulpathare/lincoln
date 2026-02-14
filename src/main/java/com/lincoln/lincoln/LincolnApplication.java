7e685e7a063c10f696c4920ecbb9333db6056b62
prafulpathare <patharepraful@gmail.com>
2026-02-14 20:54:42 +0530

    init

---
+++ src/main/java/com/lincoln/lincoln/LincolnApplication.java
@@ -0,0 +1,13 @@
+package com.lincoln.lincoln;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LincolnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LincolnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lincoln/lincoln/LincolnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lincoln.lincoln;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LincolnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
